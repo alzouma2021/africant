6b9d0ae96cb49d9895da1491e312bef598f14558
alzouma2021 <mahamadoualzouma854@gmail.com>
2023-08-07 12:19:07 +0000

    updates security project

--- src/main/java/com/africanb/africanb/utils/security/ParameterHeaderHttp.java
+++ src/main/java/com/africanb/africanb/utils/security/ParameterHeaderHttp.java
@@ -15,7 +15,8 @@ public class ParameterHeaderHttp {
         res.setHeader("Access-Control-Allow-Origin","*");
         res.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,DELETE,OPTIONS");
         res.setHeader("Access-Control-Allow-Credentials","true");
-        res.setHeader("Access-Control-Allow-Headers","Authorization, server_id, client_id");
+        res.setHeader("Access-Control-Allow-Headers","server_id, client_id");
+        res.setHeader("Access-Control-Allow-Headers","Authorization");
         res.setHeader("Access-Control-Allow-Headers","Origin, Access-Control-Allow-Methods, " +
                 "Accept, X-Requested-With, Content-Type, Access-Control-Allow-Origin, Access-Control-Request-Method," +
                 "Access-Control-Request-Headers, Show-Success-Message, Show-Loader, Show-Error-Message, sessionUser, lang, user, token, sessionuser, xsrf-token");
