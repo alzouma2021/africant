e803c100075b1226168d8c98a97a4e7067a0a4db
alzouma2021 <mahamadoualzouma854@gmail.com>
2023-08-07 12:09:58 +0000

    updates security project

--- src/main/java/com/africanb/africanb/utils/security/SecurityServletFilter.java
+++ src/main/java/com/africanb/africanb/utils/security/SecurityServletFilter.java
@@ -58,7 +58,7 @@ public class SecurityServletFilter extends HttpFilter {
         //ULRS exemptions authorization
         List<String> listUrlDoNotHaveAuthentication = Collections.synchronizedList(new ArrayList<String>());
         listUrlDoNotHaveAuthentication.add("/users/login");
-        inializationResponseParamHeaders((HttpServletResponse) response);
+        //inializationResponseParamHeaders((HttpServletResponse) response);
         //Check Options
         if (SecurityServices.checkIfRequestHasNotNeedAuthentication(request, response, chain)) return;
         try {
@@ -138,7 +138,7 @@ public class SecurityServletFilter extends HttpFilter {
         res.setHeader("Access-Control-Allow-Credentials","true");
         res.setHeader("Access-Control-Allow-Headers","Origin, Access-Control-Allow-Methods, " +
                 "Accept, X-Requested-With, Content-Type, Access-Control-Allow-Origin, Access-Control-Request-Method, Authorization," +
-                "Access-Control-Request-Headers, Show-Success-Message, Show-Loader, Show-Error-Message, sessionUser, lang, user, token, sessionuser, xsrf-token");
+                "Access-Control-Request-Headers, Show-Success-Message, Show-Loader, Show-Error-Message, sessionUser, lang, user, token, sessionuser, xsrf-token,server_id, client_id");
         res.addHeader("Access-Control-Expose-Headers", " xsrf-token");
 
     }
