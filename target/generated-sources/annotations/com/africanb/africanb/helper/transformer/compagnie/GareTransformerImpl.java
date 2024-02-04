6d24ea5338985694a9f54abcd0d2128817570b32
alzouma2021 <mahamadoualzouma854@gmail.com>
2024-02-04 13:38:03 +0000

    update

--- target/generated-sources/annotations/com/africanb/africanb/helper/transformer/compagnie/GareTransformerImpl.java
+++ target/generated-sources/annotations/com/africanb/africanb/helper/transformer/compagnie/GareTransformerImpl.java
@@ -11,7 +11,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-01T10:30:02+0000",
+    date = "2024-02-03T12:04:38+0000",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.0.1 (Oracle Corporation)"
 )
 public class GareTransformerImpl implements GareTransformer {
