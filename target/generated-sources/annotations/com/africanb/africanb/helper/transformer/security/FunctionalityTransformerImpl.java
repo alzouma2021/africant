924a7cff522ac1e0fea0affec1610c0b911ac612
alzouma2021 <mahamadoualzouma854@gmail.com>
2024-02-13 11:31:35 +0000

    update

--- target/generated-sources/annotations/com/africanb/africanb/helper/transformer/security/FunctionalityTransformerImpl.java
+++ target/generated-sources/annotations/com/africanb/africanb/helper/transformer/security/FunctionalityTransformerImpl.java
@@ -10,7 +10,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-02-03T12:04:37+0000",
+    date = "2024-02-13T11:29:41+0000",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 11.0.0.1 (Oracle Corporation)"
 )
 public class FunctionalityTransformerImpl implements FunctionalityTransformer {
